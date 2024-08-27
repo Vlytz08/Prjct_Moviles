fd8406f2fe0f2e911e9e88dd9937bbab51fb8ed6
NoHellNoHeaven <espindolaalex627@gmail.com>
2024-08-26 23:30:10 -0400

    layerIniciarSesion

--- ProyectoAppMakakoVirus/src/app/home/home.page.ts
+++ ProyectoAppMakakoVirus/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { addIcons } from 'ionicons';
+import { eye, lockClosed } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,8 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() {
+    addIcons({ eye, lockClosed })
+  }
 
 }
